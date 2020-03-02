1c36536963d1367ed2e588df00936556d24754bb
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-02 13:44:52 -0600

    adds cloudinary api

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
     title: `David Portillo`,
@@ -28,6 +30,19 @@ module.exports = {
       },
     },
     `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-source-cloudinary`,
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        resourceType: "",
+        type: ``,
+        maxResults: 500,
+        tags: true,
+        prefix: ``,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
