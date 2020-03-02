402dd6a167331562ae7bc1e78eee195071083d3c
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-02 11:49:51 -0600

    adds footer to layout

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,73 +1,132 @@
 import React from "react"
 import { Navbar, Container, Col, Row } from "react-bootstrap"
 import { Link } from "gatsby"
-import { FaFacebookF } from "react-icons/fa"
+import { FaTwitterSquare, FaFacebookSquare, FaInstagram } from "react-icons/fa"
 import { css } from "@emotion/core"
 
 const Footer = () => (
   <>
     <Navbar
-      bg="primary"
+      bg="dark"
       variant="dark"
-      className="pt-5 pb-5"
+      className="pt-5 pb-5 text-white-50"
       css={css`
-        background-color: #2f124d !important;
+        a {
+          color: #818386;
+        }
       `}
     >
       <Container>
         <Row>
           <Col md={4}>
-            <Link to="/">
-              <Navbar.Brand>Threads of Love</Navbar.Brand>
-            </Link>
-            <p className="text-white">
+            <h4>David Portillo Tenor</h4>
+            <p>All Rights Reserved</p>
+            <p>
               {" "}
               © {new Date().getFullYear()}, Built with
               {` `}
-              <a href="https://www.gatsbyjs.org" className="text-white">
-                Gatsby
-              </a>
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
             </p>
           </Col>
-          <Col md={2} className="text-white">
-            <Link className="text-white nav-link" to="/about">
-              About Us
-            </Link>
-            <Link className="text-white nav-link" to="/get-involved">
-              Volunteer
-            </Link>
-            <Link className="text-white nav-link" to="/donate">
-              Donate
-            </Link>
-          </Col>
-          <Col md={2} className="text-white">
-            <a className="text-white nav-link" href="https://facebook.com">
-              <FaFacebookF /> Facebook
-            </a>
-          </Col>
-          <Col md={4} className="text-white">
-            <address>
-              2477 N Loop 1604 East, San Antonio, TX 78232 <br />
-              c/o Linda Coward
+          <Col
+            md={5}
+            css={css`
+              line-height: 16px;
+            `}
+          >
+            <h4>Address</h4>
+            <address class="site-footer-address site-footer-p">
+              <a href="http://imgartists.com/artist/david_portillo">
+                IMG Artists, New York
+              </a>
+              <br /> Pleiades House 7 West 54th St
+              <br /> New York, NY 10019 US
+              <br /> <a href="tel:+1 212 994 3500">Tel: +1 212 994 3500</a>
+              <br />
+              <br /> Contact
+              <br />
+              <a href="mailto:ssnook@imgartists.com">
+                Sam Snook, General Management
+              </a>
+              <br />
+              <a href="tel:+1 212 994 3500">Tel: +1 212 994 3500</a>
+              <br />
+              <br />
+              <a href="mailto:rjaroff@imgartists.com">
+                Romana Jaroff, US Concert/Recital Booking
+              </a>
+              <br />
+              <a href="tel:+1 212 994 3518 ">Tel: +1 212 994 3518 </a>
             </address>
 
-            <a href="mailto:threadsoflove@gmail.com" className="text-white">
-              tolatcbc@gmail.com
+            <address class="site-footer-address site-footer-p">
+              <a href="http://www.harrisonparrott.com/artist/profile/david-portillo">
+                Harrison Parrott, London
+              </a>
+              <br /> The Ark, 201 Talgarth Road
+              <br /> London, UK W6 8BJ
+              <br />
+              <a href="tel:++44 (0) 20 7229 9166">Tel: +44 (0) 20 7229 9166</a>
+              <br />
+              <br /> Contact
+              <br />
+              <a href="mailto:ian.stones@harrisonparrott.co.uk">
+                Ian Stones, General Management
+              </a>
+              <br />
+              <a href="tel:+44 (0)20 3725 9104">Tel: +44 (0) 20 3725 9104</a>
+              <br />
+              <br />
+              <a href="mailto:alice.jones@harrisonparrott.co.uk">
+                Alice Jones, Associate
+              </a>
+              <br />
+              <a href="tel:+44 (0)20 3725 9181">Tel: +44 (0) 20 3725 9181</a>
+            </address>
+          </Col>
+          <Col md={3}>
+            <h4>Connect</h4>
+            <a href="https://www.facebook.com/davidportillotenor">
+              <FaFacebookSquare
+                size={36}
+                css={css`
+                  :hover {
+                    color: white;
+                  }
+                `}
+              />
+            </a>
+            <a href="https://twitter.com/elportillo">
+              <FaTwitterSquare
+                size={36}
+                css={css`
+                  margin-left: 10px;
+                  &:hover {
+                    color: white;
+                  }
+                `}
+              />
+            </a>
+            <a href="https://www.instagram.com/porttenor/">
+              <FaInstagram
+                size={36}
+                css={css`
+                  margin-left: 10px;
+                  &:hover {
+                    color: white;
+                  }
+                `}
+              />
             </a>
           </Col>
         </Row>
       </Container>
     </Navbar>
-    <nav
-      className="navbar navbar-expand navbar-dark bg-primary"
-      css={css`
-        background-color: #2f124d !important;
-      `}
-    >
+    <Navbar bg="dark" variant="dark">
       <a href="https://sacodersunited.com" className="navbar-brand">
         ©sacodersunited
       </a>
-    </nav>
+    </Navbar>
   </>
 )
 export default Footer
