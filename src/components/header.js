4ca0667f6878479f7278b14e9754511ac363c223
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-02 09:04:15 -0600

    builds out header

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,33 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Navbar, Nav, Container } from "react-bootstrap"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+    <Container>
+      <Link to="/">
+        <Navbar.Brand>{siteTitle}</Navbar.Brand>
+      </Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Link to="/biography" className="nav-link" activeClassName="active">
+            Biography
+          </Link>
+          <Link to="/acclaims" className="nav-link" activeClassName="active">
+            Acclaims
+          </Link>
+          <Link to="/calendar" className="nav-link" activeClassName="active">
+            Calendar
+          </Link>
+          <Link to="/media" className="nav-link" activeClassName="active">
+            Media
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
 )
 
 Header.propTypes = {
