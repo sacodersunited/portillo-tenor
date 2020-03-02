ae798cc48f34e0aed65ce56df1a37408f69fb7b6
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-02 08:16:09 -0600

    adds react bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Button from "react-bootstrap/Button"
 
 const IndexPage = () => (
   <Layout>
@@ -14,7 +15,9 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">
+      <Button>Go to page 2</Button>
+    </Link>
   </Layout>
 )
 
