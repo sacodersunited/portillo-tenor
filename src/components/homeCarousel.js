845ff66c7945b00c3e1191054249cb2071468895
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-03 13:44:03 -0600

    cleanup

--- src/components/homeCarousel.js
+++ src/components/homeCarousel.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Carousel } from "react-bootstrap"
-import { graphql } from "gatsby"
 import UseCarousel from "../hooks/use-Carousel"
 
 const HomeCarousel = () => {
