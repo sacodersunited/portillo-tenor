45a9167158e11e8d3d673260bca092fbcf580e3a
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-10 21:49:19 -0500

    starts the acclaim comp on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HomeCarousel from "../components/homeCarousel"
 import HomeCalendar from "../components/HomeCalendar"
+import HomeAcclaims from "../components/HomeAcclaims"
 
 const IndexPage = () => (
   <Layout fullWidth>
     <SEO title="Home" />
     <HomeCarousel />
     <HomeCalendar />
+    <HomeAcclaims />
   </Layout>
 )
 
