20c94534e9250d7e0aea1293547dece33e5492b1
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-06-01 00:43:16 -0500

    - adds strapi backend - moves gatsby to frontend - add events for 2020 - adds env support for gatsby and strapi integration

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c1b74659-0918-46be-88cf-d8b93b472da4'
+};
--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -50,6 +50,17 @@ module.exports = {
         prefix: ``,
       },
     },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: process.env.API_URL || "http://localhost:1337",
+        contentTypes: [
+          // List of the Content Types you want to be able to request from Gatsby.
+          "event",
+        ],
+        queryLimit: 1000,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
